5446650dd38ecb704ba1b7fe790ac0e6913a7612
Karim S <botenik1@yandex.ru>
2019-02-05 12:28:59 +0300

    first succesfull merging of the filters

--- RRC_filter_initial.m
+++ RRC_filter_initial.m
@@ -1,17 +1,40 @@
-% Generating RRC filter
+%% Generating RRC filter
 
-Nsamp = 8;           % Filter span
+Nsymb = 12;           % Filter span
 roll_off = 0.35;     % Roll-off factor
-sampsPerSymb = 8;    % Uspample/decimation factor
+sampsPerSymb = 8;    % Uspample/decimation factor 
 
 rrcTxFil = comm.RaisedCosineReceiveFilter( ...
   'Shape',                  'Square root', ...
   'RolloffFactor',          roll_off,      ...
-  'FilterSpanInSymbols',    Nsym,          ...
+  'FilterSpanInSymbols',    Nsymb,         ...
   'InputSamplesPerSymbol',  sampsPerSymb,  ...
   'DecimationFactor',       sampsPerSymb   ...
 );
 
+taps = Nsymb * sampsPerSymb + 1;
 
 fvtool(rrcTxFil, 'Analysis', 'impulse');
+
+%% Generating filter to attenuate the stopband
+
+
+FIR = Equirippple_Remez_matlab;
+RRC = RRC_matlab;
+
+RRC_Numerator = RRC.Numerator;
+FIR_Numerator = FIR.Numerator;
+
+
+
+%% Generate new filter
+% Doing the convolution of the filters
+convolution = conv(RRC_Numerator, FIR_Numerator);
+
+newFil = dsp.FIRFilter('Numerator', convolution);
+
+fvtool(newFil)
+
+
+%% Testing influence of this filter to the scatter plot
 
